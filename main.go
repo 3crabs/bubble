bd08deed84fd25076b38749a41d32e3a5a483e88
FedorovVladimir <vladimir.fodorow@gmail.com>
2021-08-22 23:25:42 +0700

    создание простой реализации сервера
    реализация всех методов сервиса задач, хранение данных организовано в оперативной памяти

--- main.go
+++ main.go
@@ -3,29 +3,68 @@ package main
 import (
 	"context"
 	pb "github.com/3crabs/bubble/task"
+	"github.com/gofrs/uuid"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
+	"google.golang.org/protobuf/types/known/timestamppb"
 	"google.golang.org/protobuf/types/known/wrapperspb"
 	"log"
 	"net"
 )
 
-type server struct{}
-
-func (s server) GetTasks(ctx context.Context, in *emptypb.Empty) (*pb.TaskInfos, error) {
-	panic("implement me")
+type server struct {
+	tasks map[string]*pb.TaskInfo
 }
 
-func (s server) AddTask(ctx context.Context, in *pb.TaskCreateInfo) (*wrapperspb.StringValue, error) {
-	panic("implement me")
+func (s *server) GetTasks(_ context.Context, _ *emptypb.Empty) (*pb.TaskInfos, error) {
+	var tasks []*pb.TaskInfo
+	if s.tasks == nil {
+		s.tasks = make(map[string]*pb.TaskInfo)
+	}
+	for _, task := range s.tasks {
+		tasks = append(tasks, task)
+	}
+	return &pb.TaskInfos{Tasks: tasks}, status.New(codes.OK, "").Err()
 }
 
-func (s server) DoneTask(ctx context.Context, in *wrapperspb.StringValue) (*emptypb.Empty, error) {
-	panic("implement me")
+func (s *server) AddTask(_ context.Context, in *pb.TaskCreateInfo) (*wrapperspb.StringValue, error) {
+	out, err := uuid.NewV4()
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "error while generating task id: %v", err)
+	}
+	if s.tasks == nil {
+		s.tasks = make(map[string]*pb.TaskInfo)
+	}
+	task := pb.TaskInfo{Id: out.String(), Name: in.Name, Created: timestamppb.Now(), Done: false}
+	s.tasks[task.Id] = &task
+	return wrapperspb.String(task.Id), status.New(codes.OK, "").Err()
 }
 
-func (s server) DeleteTask(ctx context.Context, in *wrapperspb.StringValue) (*emptypb.Empty, error) {
-	panic("implement me")
+func (s *server) DoneTask(_ context.Context, in *wrapperspb.StringValue) (*emptypb.Empty, error) {
+	if s.tasks == nil {
+		s.tasks = make(map[string]*pb.TaskInfo)
+	}
+	value, exists := s.tasks[in.Value]
+	if exists {
+		return &emptypb.Empty{}, status.New(codes.NotFound, "task not found").Err()
+	}
+	value.Done = true
+	s.tasks[value.Id] = value
+	return &emptypb.Empty{}, status.New(codes.OK, "").Err()
+}
+
+func (s *server) DeleteTask(_ context.Context, in *wrapperspb.StringValue) (*emptypb.Empty, error) {
+	if s.tasks == nil {
+		s.tasks = make(map[string]*pb.TaskInfo)
+	}
+	value, exists := s.tasks[in.Value]
+	if exists {
+		return &emptypb.Empty{}, status.New(codes.NotFound, "task not found").Err()
+	}
+	s.tasks[value.Id] = nil
+	return &emptypb.Empty{}, status.New(codes.OK, "").Err()
 }
 
 func main() {
